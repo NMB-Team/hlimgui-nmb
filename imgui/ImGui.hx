9be8c6928c790c740f0ba205a3ad692767e62be7
dtwotwo <dprojetmail@gmail.com>
2026-08-04 12:48:31 +0300

    fix docking feature
    after updating from old imgui to newer, they moved to bit 7, bit 6 means `NoKeybord` now

--- imgui/ImGui.hx
+++ imgui/ImGui.hx
@@ -385,9 +385,10 @@ enum abstract ImGuiConfigFlags(Int) from Int to Int {
     var NavNoCaptureKeyboard   = 1 << 3;   // Instruct navigation to not set the io.WantCaptureKeyboard flag when io.NavActive is set.
     var NoMouse                = 1 << 4;   // Instruct imgui to clear mouse position/buttons in NewFrame(). This allows ignoring the mouse information set by the backend.
     var NoMouseCursorChange    = 1 << 5;   // Instruct backend to not alter mouse cursor shape and visibility. Use if the backend cursor changes are interfering with yours and you don't want to use SetMouseCursor() to change mouse cursor. You may want to honor requests from imgui by reading GetMouseCursor() yourself instead.
+    var NoKeyboard             = 1 << 6;   // Instruct imgui to disable keyboard inputs and interactions.
 
     // [BETA] Docking
-    var DockingEnable          = 1 << 6;   // Docking enable flags.
+    var DockingEnable          = 1 << 7;   // Docking enable flags.
 
     // [BETA] Viewports
     // When using viewports it is recommended that your default value for ImGuiCol_WindowBg is opaque (Alpha=1.0) so transition to a viewport won't be noticeable.
